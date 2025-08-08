fa24f83412f6b4fe60602e36bf79b643dc530ce0
NinnjA254 <quaypeter@yahoo.com>
2025-08-08 16:46:00 +0300

    improved toggleterm layout switching

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -2,6 +2,12 @@ local config = function(_, opts)
   local tt = require 'toggleterm'
   tt.setup(opts)
   local trim_spaces = false
+  vim.keymap.set('n', '<f10>', ':ToggleTerm direction=float<CR>', { silent = true })
+  vim.keymap.set('t', '<f10>', '<C-\\><C-n>:ToggleTerm direction=float<CR>', { silent = true })
+
+  vim.keymap.set('n', '<f9>', ':ToggleTerm direction=horizontal<CR>', { silent = true })
+  vim.keymap.set('t', '<f9>', '<C-\\><C-n>:ToggleTerm direction=horizontal<CR>', { silent = true })
+
   vim.keymap.set('v', '<leader>s', function()
     tt.send_lines_to_terminal('visual_selection', trim_spaces, { args = vim.v.count })
   end)
--- remember.md
+++ remember.md
@@ -1,4 +1,10 @@
 # Remember!!
+## something
+you can create keymaps to a callback and do whatever the hell you want!!
+you can create keymaps to whatever sequence of characters you want and neovim seems to interpret them as if you typed them out manually!! eg:
+```lua
+  vim.keymap.set('n', '<f10>', ':ToggleTerm direction=float<CR>:lua print("hello from the toggi")<CR>')
+```
 ## These are cool things I found in the neovim documentation that are worth remembering.
 :set <vim option>& resets the vim option to its default value
 eg. :set tabstop& resets tabstop to 8(the default value)
